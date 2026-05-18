d1e11f6ecb1b19105a5886c807ecf6ae1a1b56d0
simonzvonik <simon.zvonik@gmail.com>
2026-05-18 13:47:38 +0200

    backend logic

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://simon:simon123@cluster0.sfkoemu.mongodb.net/music-finder?retryWrites=true&w=majority'), 
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
