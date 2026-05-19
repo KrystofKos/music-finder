54773a12e6b517ac042049a4580c2ebfa5a45c6e
simonzvonik <simon.zvonik@gmail.com>
2026-05-19 11:12:37 +0200

    Artist database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ArtistsModule } from './artists/artists.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://simon:simon123@cluster0.sfkoemu.mongodb.net/music-finder?retryWrites=true&w=majority'), 
-    UsersModule,
+    UsersModule, ArtistsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
