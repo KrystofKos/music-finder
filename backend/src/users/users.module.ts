300469ac1e805b37f48eeee0ee972e1410583f3a
egorkazabey <visowiacsgo@gmail.com>
2026-05-26 10:52:10 +0200

    Revert "Backend live chat"

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './users.schema'; // Tvoje schéma uživatele
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 import { User, UserSchema } from './users.schema';
 import { USERS_REPO } from './users.repo';
 import { MongoUsersRepo } from './mongo-users.repo';
@@ -10,7 +9,6 @@ import { MongoUsersRepo } from './mongo-users.repo';
 // users.module.ts
 @Module({
   imports: [
-    // Tady se uživatelé vážou na MongoDB
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UsersController],
